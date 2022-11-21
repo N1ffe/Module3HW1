de46e98ad693a2402c7130f006daf56a3bf888c2
Niffe <petrovnik01@ukr.net>
2022-11-21 16:37:20 +0200

    Task

--- Module3HW1/Module3HW1/Program.cs
+++ Module3HW1/Module3HW1/Program.cs
@@ -4,7 +4,27 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CustomCollection<int> c = new CustomCollection<int>();
+            int[] testArr = new int[] { 8, 8, 8 };
+            List<int> testC = new List<int> { 9, 9, 9 };
+            for (int i = 0; i < 4; i++)
+            {
+                c.Add(i + 1);
+            }
+            c.AddRange(testArr);
+            c.AddRange(testC);
+            c.RemoveAt(9);
+            c.Remove(3);
+            foreach (int item in c)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            c.Sort(true);
+            foreach (int item in c)
+            {
+                Console.Write($"{item} ");
+            }
         }
     }
 }
